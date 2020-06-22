6c39fab3a8eb86a8c7aa36e5e3d489845a65e83b
Muresan Alexandru <alex@Muresans-MacBook-Pro.local>
2020-06-22 22:21:26 +0300

    Added slowdown and scaling and fixed gif creation
    Video is scaled to 70% of original size so that it can fit inside
    the screen better.
    
    Added a few sleeps to slow down the video so it's easier to spot
    mistakes.
    
    Prior to this commit the gifs were stacked on top of eachother
    (e.g. 1st gif was good, 2nd gif was 1st + frames that should be in
    the second one and so on). This was fixed in this commit.

--- video_editor.py
+++ video_editor.py
@@ -7,9 +7,19 @@ import cv2
 import imageio
 
 def onChange(trackbarValue):
+    global play_video
     if play_video:
         cap.set(cv2.CAP_PROP_POS_FRAMES,trackbarValue)
-        err,img = cap.read()
+        _,img = cap.read()
+
+        scale_percent = 70 # percent of original size
+        width = int(img.shape[1] * scale_percent / 100)
+        height = int(img.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        # resize image
+        img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+
+        time.sleep(0.03)
         cv2.imshow("Video", img)
         pass
 
@@ -17,10 +27,13 @@ def onChange(trackbarValue):
 end = 10e-5
 
 cv2.namedWindow('Video')
+cv2.moveWindow('Video', 10, 40)
+
 gathering = False
 play_video = True
 
 frames_for_gif = []
+
 
 print("left and right arrows - scroll through video\n"
       "space or 'p' - pause and play the video\n"
@@ -32,7 +45,7 @@ print("left and right arrows - scroll through video\n"
 # At the moment it is given as the first parameter 
 # in the command line
 video_path = pathlib.Path(sys.argv[1])
-path_parent = video_path.parent
+path_parent = video_path.parent / "gifs"
 print(f"Loaded video from: {video_path}")
 print(f"Gifs will be saved to: {path_parent}\n")
 
@@ -43,7 +56,7 @@ cv2.createTrackbar("Progress", "Video", 0, int(total_frames), onChange)
 while cap.isOpened():
     start = time.time()
     if play_video:
-        err,img = cap.read()
+        _, img = cap.read()
     
     if gathering == True:
         frames_for_gif.append(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
@@ -52,6 +65,8 @@ while cap.isOpened():
     current_frame = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
     cv2.setTrackbarPos("Progress", "Video", current_frame)
 
+    # if k != 255:
+    #     print(k)
     if k == 27:
         break
     if k == ord('p') or k == 32:
@@ -59,9 +74,9 @@ while cap.isOpened():
         # cv2.waitKey(-1)
         play_video = not play_video
     if k == 81 or k == 123:
-        cv2.setTrackbarPos("Progress", "Video", current_frame - 30)
+        cv2.setTrackbarPos("Progress", "Video", current_frame - 50)
     if k == 83 or k == 124:
-        cv2.setTrackbarPos("Progress", "Video", current_frame + 30)
+        cv2.setTrackbarPos("Progress", "Video", current_frame + 50)
     if k == ord('s'):
         start_frame = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
         gathering = True
@@ -73,8 +88,16 @@ while cap.isOpened():
         export_path = path_parent / export_name
         print(f"Saving gif as {export_name}...")
         imageio.mimsave(export_path, frames_for_gif, 'GIF')
+        frames_for_gif = []
 
     if play_video:
+        scale_percent = 70 # percent of original size
+        width = int(img.shape[1] * scale_percent / 100)
+        height = int(img.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        # resize image
+        img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA) 
+        time.sleep(0.03)
         cv2.imshow("Video", img)
         print(f"{int(current_frame)} / {int(total_frames)} - {int(1/end)} fps   ", end='\r')
 
